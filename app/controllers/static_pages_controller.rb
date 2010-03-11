4694342492399fdcaa5d757b804a1c4903efad3a
Albus Dumbledore <Dumbledore@albus-dumbledores-mac-pro.local>
2010-03-10 19:49:09 -0500

    static pages

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,8 +1,7 @@
 class StaticPagesController < ApplicationController
 
   def show
-    page = params[:id]
-    render :template => "static_pages/#{params[:id]}"
+    render :action => params[:page]
   end
 
 end
